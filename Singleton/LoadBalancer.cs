80aba2c7a6d040c6bcb52d32c0917ec25ef6e937
Statkeivch Roman <romanstat@mail.ru>
2021-04-19 18:11:06 +0300

    Implemented Singleton

---
+++ Singleton/LoadBalancer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Singleton
+{
+    class LoadBalancer
+    {
+        public static LoadBalancer _instance;
+        private readonly List<string> _servers = new();
+        private readonly Random _random = new();
+
+        private static object syncLock = new();
+
+        protected LoadBalancer()
+        {
+            _servers.Add("ServerI");
+            _servers.Add("ServerII");
+            _servers.Add("ServerIII");
+            _servers.Add("ServerIV");
+            _servers.Add("ServerV");
+        }
+
+        public static LoadBalancer GetLoadBalancer()
+        {
+            if (_instance != null) return _instance;
+
+            lock (syncLock)
+            {
+                _instance ??= new LoadBalancer();
+            }
+
+            return _instance;
+        }
+
+        public string Server => _servers[_random.Next(_servers.Count)];
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lb1 = LoadBalancer.GetLoadBalancer();
+            var lb2 = LoadBalancer.GetLoadBalancer();
+            
+            if (lb1 == lb2)
+            {
+                Console.WriteLine("Same instance\n");
+            }
+            
+            var balancer = LoadBalancer.GetLoadBalancer();
+
+            for (var i = 0; i < 15; i++)
+            {
+                var server = balancer.Server;
+                Console.WriteLine("Dispatch Request to: " + server);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
