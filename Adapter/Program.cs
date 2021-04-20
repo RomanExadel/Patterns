735806c1c4bd6981e47540f7e829189010c14096
Statkeivch Roman <romanstat@mail.ru>
2021-04-20 20:47:06 +0300

    Implemented Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new Driver();
+
+            var auto = new Auto();
+            var camel = new CamelToTransportAdapter(new Camel());
+
+            driver.Travel(auto);
+            driver.Travel(camel);
+        }
+    }
+}
