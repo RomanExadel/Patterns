bd2d6b4f50c9b94a0b6c4a11bb0d5a7a3f0506e7
Statkeivch Roman <romanstat@mail.ru>
2021-04-28 17:11:07 +0300

    Implemented Flyweight

---
+++ Flyweight/CharacterFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Flyweight
+{
+    class CharacterFactory
+    {
+        private readonly Dictionary<char, Character> _characters =
+            new();
+
+        public Character GetCharacter(char key)
+        {
+            Character character;
+
+            if (_characters.ContainsKey(key))
+            {
+                character = _characters[key];
+            }
+            else
+            {
+                character = key switch
+                {
+                    'A' => new CharacterA(),
+                    'B' => new CharacterB(),
+                    _ => null
+                };
+                _characters.Add(key, character);
+            }
+
+            return character;
+        }
+    }
+}
---
+++ Flyweight/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = "AB".ToCharArray();
+
+            var characterFactory = new CharacterFactory();
+
+            var pointSize = 10;
+
+            foreach (var @char in chars)
+            {
+                var character = characterFactory.GetCharacter(@char);
+                character.Display(pointSize);
+                pointSize++;
+            }
+        }
+    }
+}
